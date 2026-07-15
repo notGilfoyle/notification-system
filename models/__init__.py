9ce13b9210908786e897940025352d32eb9e45a6
Roshan Sahu <iamroshannn@gmail.com>
2026-07-15 23:17:49 +0530

    uncommit comment

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,3 @@
 from .enums import NotificationChannel, NotificationStatus
 from .request import NotificationRequest
 from .job import NotificationJob
-#this file is used to import all the models in the models package so that they can be easily imported from other modules.
